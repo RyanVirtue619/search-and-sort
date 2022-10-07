e33ea56ada14f74c5daf5bd5617b926d99ab7d1b
Ryan Virtue <ryan.virtue@mrjaffesclass.com>
2022-10-07 13:01:59 -0700

    marge

--- sort/Merge.java
+++ sort/Merge.java
@@ -10,33 +10,81 @@ import java.util.Arrays;
  */
 public class Merge
 {
-  
-  /**
-   * Returns the sorted array
-   */
-  public static int[] sort(int[] arr) {
-    // Your algorithm goes here!
 
-  }
-  
-  public static void main(String[] args) {
-    int[] arr = {53,85,93,25,39,27,42,5,24,45,33,51,5,80,4,7,91,
-      31,66,71,32,19,79,58,61,82,89,63,7,4,50,10,48,24,75,19,22,
-      73,54,51,25,33,20,52,79,97,70,54,63,49};    
-    
-    // Test the sort
-    testSort(sort(arr));
-  }
-  
-  public static void testSort(int[] arr) {
-    for (int i=0; i<arr.length-1; i++) {
-      if (arr[i] > arr[i+1]) {
-        System.out.println("FAIL at index "+i);
-        System.out.println(Arrays.toString(arr));
-        return;
-      }
+    /**
+     * Returns the sorted array
+     */
+    public static int[] sort(int[] arr) {
+        int[] ar = arr;
+        int N = ar.length;
+        if(N <= 1) return ar;
+        int middle = N/2;
+        int leftLength = middle;
+        int rightLength = N - leftLength;
+        int index = 0;
+        int[] left = new int[leftLength];
+        int[] right = new int[rightLength];
+        while(index < middle) {
+            left[index] = ar[index];
+            index++;
+        }
+        int rightIndex = 0;
+        index = middle;
+        while(index<N) {
+            right[rightIndex] = ar[index];
+            rightIndex++;
+            index++;
+        }
+        System.out.println();
+        for(int i = 0; i < left.length-1; i++) {
+            System.out.print(left[i] + " ");
+        }
+        for(int i = 0; i < right.length-1; i++) {
+            System.out.print(right[i] + " ");
+        }
+        sort(left);
+        sort(right);
+        
+        Merge(left, right);
+        return marged;
     }
-    System.out.println("SUCCESS!");
-  }
+    
+    public static int[] Merge(int[] left, int[] right) {
+        int[] marged = new int[left.length + right.length];
+        int indexLeft = 0;
+        int indexRight = 0;
+        int index = 0;
+        while(indexLeft < left.length-1 && indexRight < right.length-1) {
+           if(left[indexLeft] > right[indexRight]) {
+               marged[index] = left[indexLeft];
+               indexLeft++;
+           } else {
+               marged[index] = right[indexRight];
+               indexRight++;
+           }
+           index++;
+        }
+        return marged;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {53,85,93,25,39,27,42,5,24,45,33,51,5,80,4,7,91,
+                31,66,71,32,19,79,58,61,82,89,63,7,4,50,10,48,24,75,19,22,
+                73,54,51,25,33,20,52,79,97,70,54,63,49};    
+
+        // Test the sort
+        testSort(sort(arr));
+    }
+
+    public static void testSort(int[] arr) {
+        for (int i=0; i<arr.length-1; i++) {
+            if (arr[i] > arr[i+1]) {
+                System.out.println("FAIL at index "+i);
+                System.out.println(Arrays.toString(arr));
+                return;
+            }
+        }
+        System.out.println("SUCCESS!");
+    }
 
 }
