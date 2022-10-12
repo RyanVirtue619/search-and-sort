bc5a6ef31d817293e44cad38537f5b81c29fe25b
Ryan V <ryan.virtue@mrjaffesclass.com>
2022-10-11 19:08:26 -0700

    finishedgit add .!

--- search/Sequential.java
+++ search/Sequential.java
@@ -14,8 +14,10 @@ public class Sequential
    * Returns the index of the target value, or -1 if not found
    */
   public static int search(int[] arr, int target) {
-    // Your algorithm goes here!
-    
+    for(int i = 0; i < arr.length; i++) {
+        if(arr[i] == target) return i;
+    }
+    return -1;
   }
   
   public static void main(String[] args) {
