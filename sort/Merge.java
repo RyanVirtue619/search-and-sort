d0a6d7b6eaed2d181e8839dc46af63cf763d2a1d
Ryan V <ryan.virtue@mrjaffesclass.com>
2022-10-11 18:32:08 -0700

    finished!

--- sort/Merge.java
+++ sort/Merge.java
@@ -15,40 +15,59 @@ public class Merge
      * Returns the sorted array
      */
     public static int[] sort(int[] arr) {
-        int[] ar = arr;
-        int N = ar.length;
-        if(N <= 1) return ar;
-        int middle = N/2;
-        int leftLength = middle;
-        int rightLength = N - leftLength;
-        int index = 0;
-        int[] left = new int[leftLength];
-        int[] right = new int[rightLength];
-        return ar;
-    }
-    
-    public static int[] mergeSort(int[] arr, int left, int middle, int right) {
-        
-    }
-    
-    public static 
-    
-    public static int[] Merge(int[] left, int[] right) {
-        int[] marged = new int[left.length + right.length];
-        int indexLeft = 0;
-        int indexRight = 0;
-        int index = 0;
-        while(indexLeft < left.length-1 && indexRight < right.length-1) {
-           if(left[indexLeft] > right[indexRight]) {
-               marged[index] = left[indexLeft];
-               indexLeft++;
-           } else {
-               marged[index] = right[indexRight];
-               indexRight++;
-           }
-           index++;
+        int length = arr.length;
+
+        if(length < 2) {
+            return arr;
         }
-        return marged;
+
+        int midIndex = length / 2;
+        int[] leftHalf = new int[midIndex];
+        int[] rightHalf = new int [length - midIndex];
+
+        for(int i = 0; i < midIndex; i++) {
+            leftHalf[i] = arr[i];
+        }
+        for(int i = midIndex; i < length; i++) {
+            rightHalf[i - midIndex] = arr[i];
+        }
+
+        sort(leftHalf);
+        sort(rightHalf);
+        Merge(arr, leftHalf, rightHalf);
+        return arr;
+    }
+    
+    public static int[] Merge(int[] arr, int[] left, int[] right) {
+        int leftSize = left.length;
+        int rightSize = right.length;
+
+        int a = 0, b = 0, c = 0;
+
+        while (a < leftSize && b < rightSize) {
+            if (left[a] <= right[b]) {
+                arr[c] = left[a];
+                a++;
+            } else {
+                arr[c] = right[b];
+                b++;
+            }
+            c++;
+        }
+        
+        while(a < leftSize) {
+            arr[c] = left[a];
+            a++;
+            c++;
+        }
+        
+        while(b < rightSize) {
+            arr[c] = right[b];
+            b++;
+            c++;
+        }
+        
+        return arr;
     }
 
     public static void main(String[] args) {
